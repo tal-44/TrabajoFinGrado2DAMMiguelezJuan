77cd041326b86419fa83b91b8e6a4eadc07c044e
Juan <juanmiguelez2005@gmail.com>
2025-05-26 01:05:54 +0100

    commit periodico 26/05/2025

--- ESPortiba/backend/src/main/java/tfg/backend/infrastructure/rest/OrderController.java
+++ ESPortiba/backend/src/main/java/tfg/backend/infrastructure/rest/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/orders")
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 public class OrderController {
     private final OrderService orderService;
